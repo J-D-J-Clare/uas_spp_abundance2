026fdc0e69a5ad1f71f9d14b259ad336a705d8ed
[Andrii Zaiats] <[andriizaiats@u.boisestate.edu]>
2023-03-17 16:14:55 -0600

    n-mix update. site-level covars

--- intensity_cells_model.R
+++ intensity_cells_model.R
@@ -1,27 +1,31 @@
 pkgs <- c("tidyverse", "sf", "rstan", "terra", 'nimble')
 sapply(pkgs, require, character.only = TRUE)
 scalefn <- function(x) {(x - mean(x))/(2 * sd(x))}
+`%notin%` <- negate(`%in%`)
 # ---
 
 # === data is currently set up to attempt capture-recapture
-# n_obs: number of drone detected plants 
-# ngt25: number of field mapped plants taller than 25 cm
-# n: number of field mapped plants
-# nTP: number of field mapped + detected with UAS
-# nTPM: number of field mapped + detected + not NTP b/c 1 segment = 2 field points
-# nFP: number of field mapped + detected + classified as ARTR but not in fact ARTR
-# can be assumed to be absent, since we have only 17 plants across all sites
-# nM: number of field plants not detected with UAS
-# nU: number of plants detected w/ UAS, but unknown validation status
+# note: only > 25 cm plants considered & classified as ARTR from random-forest
+# see power point for 'rules' of counts
+# TP: count of field mapped + matched with UAS (verified cells only)
+# FP: count of UAS polygons not matched with field pts (verified cells only)
+# FN: count of field mapped plant + not detected with UAS (verified cells only)
+# Unk: count of plants detected w/ UAS (unvalidated cells only)
 
-
-# load cell-level data
-dat0 <- read.csv("data/celldat_full.csv")  |>
+# load covariates
+datcov <- read.csv("data/celldat_covar.csv") |> 
   mutate(dist_sq = dist^2) |>
-  filter(!is.na(tpi) | !is.na(slope))  # |> filter(val == 1) |> select(-val)
+  filter(!is.na(tpi) | !is.na(slope)) 
+
+# load counts
+datct <- read.csv("data/celldat_match_counts.csv") |> 
+  filter(ucid %in% datcov$ucid)
+
+dat0 <- datcov |> 
+  bind_cols(datct |> select(TP, FN, FP, Unk))
 
 # ---
-site <- unique(dat0$site)[1]
+site <- unique(dat0$site)[3]
 # ---
 # === covariates
 # het3: structural heterogeneity at coarse scale
@@ -37,7 +41,7 @@ site <- unique(dat0$site)[1]
 
 # --- add standardized predictors
 dat0 |> 
-  filter(site == !!site) |>
+  filter(site == !!site, burnt == 0) |> #, 
   mutate(across(c(het3, het6, het8, tpi, slope, elev, max.ht, avg.ht, dist, dist_sq), 
                 scalefn, .names = "{.col}_sc")) -> dat
 
@@ -76,38 +80,40 @@ Mod<-nimbleCode({
     log(mu[j, 1]) <- inprod(beta[1:nxf, 1], XNaive[j, 1:nxf] )  # FP
     log(mu[j, 2]) <- inprod(beta[1:nxf, 2], XNaive[j, 1:nxf] )  # TP
     NTP[j]~dpois(mu[j, 2])
+    
+    # n[j]~dpois(p*mu[j, 2] + mu[j, 1])
     n[j]~dCompoundNMix(N=NTP[j], p=p, lamFP=mu[j, 1])
   }
   
   for (j in 1:length(nVal)) {
-    mu2[j, 1] <- exp(inprod(beta[1:nxf,1], XVal[j,1:nxf]))
-    nTP[j]~dpois(mu2[j, 2])
-    mu2[j, 2] <- exp(inprod(beta[1:nxf,2], XVal[j,1:nxf]))
+    log(mu2[j, 1]) <- inprod(beta[1:nxf,1], XVal[j,1:nxf]) # FP
     nFP[j]~dpois(mu2[j, 1])
+    log(mu2[j, 2]) <- inprod(beta[1:nxf,2], XVal[j,1:nxf]) # TP
+    nTP[j]~dpois(mu2[j, 2])
+    FN[j]~binom(1-p, nTP[j])
   }
   
   ###validated plants...
   ###guess at the number of unvalided shrubs that exist
   EN<-sum(mu[1:nNaive, 2])+sum(mu2[1:nVal, 2])
   N<-sum(NTP[1:nNaive])+sum(nTP[1:nVal])
+  NVal <- sum(mu2[1:nVal, 2])
 })
 
 Data <- list(
-  n = dat |> filter(val == 0) |> pull(n_obs),
-  nTP = dat |> filter(val == 1) |> pull(ngt25),
-  nFP = dat |> filter(val == 1) |> 
-    mutate(ns = n_obs - ngt25, 
-           ns = ifelse(ns < 0, 0, ns)) |> pull(ns),
+  nTP = dat |> mutate(nTP = TP + FN) |> filter(val == 1) |> pull(nTP),
+  nFP = dat |> filter(val == 1) |> pull(FP),
+  n = dat |> filter(val == 0) |> pull(Unk),
   
   XNaive= dat |> filter(val == 0) |> 
     mutate(Int = 1) |>
-    select(Int, burn, het3_sc, het8_sc, max.ht_sc, dist_sc) |> as.matrix(),
+    select(Int, max.ht_sc) |> as.matrix(),
   XVal= dat |> filter(val == 1) |> 
     mutate(Int = 1) |>
-    select(Int, burn, het3_sc, het8_sc, max.ht_sc, dist_sc) |> as.matrix()
+    select(Int, max.ht_sc) |> as.matrix()
   )
 
-Constants <- list(  nxf  = 6, 
+Constants <- list(  nxf  = 2, 
                     nNaive = nrow(dat) - sum(dat$val), 
                     nVal = sum(dat$val) )
 
@@ -123,37 +129,51 @@ Shrub<- nimbleModel(code = Mod, name = 'Shrub', constants = Constants,
                     data=Data, inits = Inits)
 
 ##monitors--what things to trace
-ShrubConf<- configureMCMC(Shrub, monitors = c("beta", "N", "EN"))
+ShrubConf<- configureMCMC(Shrub, monitors = c("beta", "N", "EN", "NVal", "p"), 
+                          onlySlice = FALSE)
 
 
 Rmcmc <- buildMCMC(ShrubConf)
 compMCMC <- compileNimble(Rmcmc, Shrub)
 
-samps <- runMCMC(mcmc = compMCMC$Rmcmc,
-               niter = 50000, nburnin = 40000, thin = 10, 
-               nchains = 1, samplesAsCodaMCMC = TRUE)
+samp <- runMCMC(mcmc = compMCMC$Rmcmc,
+               niter = 100000, nburnin = 50000, thin = 100, 
+               nchains = 4, samplesAsCodaMCMC = TRUE)
+# ---
+library(MCMCvis)
+MCMCtrace(samp, pdf = FALSE, params = "beta")
+MCMCtrace(samp, pdf = FALSE, params = "p")
+MCMCtrace(samp, pdf = FALSE, params = c("EN", "N","NVal") )
+cat("nTP: ", sum(Data$nTP))
+cat("nUnk: ", sum(Data$n))
+# ---
 
-pred <- rpois(Constants$nVal,
-              exp( Data$XVal %*% apply(samps[,1:Constants$nxf], 2, mean) ) + 
-                exp(Data$XVal %*% apply(samps[,(Constants$nxf+1):(2*ncol(Data$XVal))], 2, mean) ) ) 
-pred <- exp(Data$XVal %*% apply(samps[,1:Constants$nxf], 2, mean)) + 
-  exp(Data$XVal %*% apply(samps[,(Constants$nxf+1):(2*ncol(Data$XVal))], 2, mean) )
-plot(log(pred) ~ log(Data$n), pch = 19, cex = .5)
+samps <- map(samp, as.data.frame) |> bind_rows()
+pred <- exp(Data$XVal %*% apply(samps[,6:7], 2, mean))
+plot(pred ~ (Data$nTP), pch = 19, cex = .5)
 abline(0, 1)
 cor(pred, Data$nTP)^2
 
-dat |> 
+dat |>
   filter(val == 0) |>
-  mutate(pred = #rpois(Constants$nNaive, 
-                      exp(Data$XNaive[,1:2] %*% apply(samps[,3:4], 2, mean)) ) |>
+  mutate(pred = exp(Data$XNaive %*% apply(samps[,6:7], 2, mean)) ) |>
   bind_rows(dat |> 
               filter(val == 1) |>
-              mutate(pred = #rpois(Constants$nVal, 
-                                  exp(Data$XVal[,1:2] %*% apply(samps[,1:2], 2, mean)) )) |>
+              mutate(pred = exp(Data$XVal %*% apply(samps[,6:7], 2, mean)) )) |>
   st_as_sf(coords = c("east", "north"), crs = 32611) |>
-  # mutate(nTP = log(nTP)) |>
   filter(site == !!site) -> out
 
-plot(out[,'pred'], pch = 19)
-plot(out[,'nTP'], pch = 19)
+plot(stars::st_as_stars(out[,'pred']), pch = 19)
+plot(out[,'TP'], pch = 19)
+
+# === 
+pred <- exp(apply(samps[,6:7], 1, function(x) { Data$XVal %*% x } ) )
+pred |> 
+  as.data.frame() |> 
+  mutate(nTP = Data$nTP, burnt = Data$XVal[,2], id = 1:nrow(Data$XVal) ) |>
+  pivot_longer(cols = contains("V")) |> select(-name) |>
+  group_by(id) |> summarize_all(median) |> ungroup() |>
+  ggplot(aes(x = nTP, y = value, colour = burnt)) + geom_point(alpha = .5)  
+  # geom_abline(intercept = 0, slope = 1)
+
 
